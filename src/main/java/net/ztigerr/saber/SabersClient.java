4251b154de9cc1e18451512221d16ca6a3f9e01c
ztigerR <regitz66@gmail.com>
2024-12-31 15:26:13 -0500

    Initial commit

---
+++ src/main/java/net/ztigerr/saber/SabersClient.java
@@ -0,0 +1,10 @@
+package net.ztigerr.saber;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SabersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/ztigerr/saber/SabersDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ztigerr.saber;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SabersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
