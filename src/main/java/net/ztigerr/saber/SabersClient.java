99892c59a0c8fe299c1992a304c503978008e19d
ztigerR <regitz66@gmail.com>
2025-01-02 16:37:07 -0500

    NonBlock blocks Tutorial

--- src/main/java/net/ztigerr/saber/SabersClient.java
+++ src/main/java/net/ztigerr/saber/SabersClient.java
@@ -1,10 +1,14 @@
 package net.ztigerr.saber;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.ztigerr.saber.block.ModBlocks;
 
 public class SabersClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_TRAPDOOR, RenderLayer.getCutout());
     }
 }
--- src/main/java/net/ztigerr/saber/item/ModItemGroups.java
+++ src/main/java/net/ztigerr/saber/item/ModItemGroups.java
@@ -18,16 +18,34 @@ public class ModItemGroups {
                     .entries((displayContext, entries) -> {
                         entries.add(ModItems.PINK_GARNET);
                         entries.add(ModItems.RAW_PINK_GARNET);
-                        entries.add(ModBlocks.PINK_GARNET_BLOCK);
-                        entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
-                        entries.add(ModBlocks.PINK_GARNET_ORE);
-                        entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
-                        entries.add(ModBlocks.MAGIC_BLOCK);
                         entries.add(ModItems.CHISEL);
                         entries.add(ModItems.CAULIFLOWER);
                         entries.add(ModItems.STARLIGHT_ASHES);
                     })
                     .build());
+    public static final ItemGroup PINK_GARNET_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP, Identifier.of(
+            Sabers.MOD_ID, "pink_garnet_blocks"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModBlocks.PINK_GARNET_BLOCK))
+                    .displayName(Text.translatable("itemgroup.sabers.pink_garnet_blocks"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModBlocks.PINK_GARNET_BLOCK);
+                        entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+
+                        entries.add(ModBlocks.PINK_GARNET_ORE);
+                        entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+
+                        entries.add(ModBlocks.MAGIC_BLOCK);
+
+                        entries.add(ModBlocks.PINK_GARNET_STAIRS);
+                        entries.add(ModBlocks.PINK_GARNET_SLAB);
+                        entries.add(ModBlocks.PINK_GARNET_BUTTON);
+                        entries.add(ModBlocks.PINK_GARNET_PRESSURE_PLATE);
+                        entries.add(ModBlocks.PINK_GARNET_FENCE);
+                        entries.add(ModBlocks.PINK_GARNET_FENCE_GATE);
+                        entries.add(ModBlocks.PINK_GARNET_WALL);
+                        entries.add(ModBlocks.PINK_GARNET_DOOR);
+                        entries.add(ModBlocks.PINK_GARNET_TRAPDOOR);
+                    }).build());
 
     public static void registerItemGroups() {
         Sabers.LOGGER.info("Registering Item Groups for " + Sabers.MOD_ID);
