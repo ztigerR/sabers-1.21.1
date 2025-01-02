b25bd9c53d6abfa4326cb1a6c923eeb8ae5a5f8b
ztigerR <regitz66@gmail.com>
2025-01-02 10:26:22 -0500

    Recipes tutorial

--- src/main/java/net/ztigerr/saber/Sabers.java
+++ src/main/java/net/ztigerr/saber/Sabers.java
@@ -21,6 +21,8 @@ public class Sabers implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		//the list of minecraft recipes is under external libraries minecraft merged two up from fabricmc_yarn wall
 		ModItemGroups.registerItemGroups();
 		ModItems.regitsterModItems();
 		ModBlocks.registerModBlocks();
