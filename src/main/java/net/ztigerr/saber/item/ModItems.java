5375faf97737befaa409ea89fec8277f959002ca
ztigerR <regitz66@gmail.com>
2025-01-02 14:04:56 -0500

    Custom Tooltips Tutorial

--- src/main/java/net/ztigerr/saber/item/ModItems.java
+++ src/main/java/net/ztigerr/saber/item/ModItems.java
@@ -3,11 +3,16 @@ package net.ztigerr.saber.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.ztigerr.saber.Sabers;
 import net.ztigerr.saber.item.custom.ChiselItem;
+
+import java.util.List;
 
 public class ModItems {
     public static final Item PINK_GARNET = regitsterItem("pink_garnet", new Item(new Item.Settings()));
@@ -15,7 +20,13 @@ public class ModItems {
 
     public static final Item CHISEL = regitsterItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
 
-    public static final Item CAULIFLOWER = regitsterItem("cauliflower", new Item(new Item.Settings().food(ModFoodComponents.CAULIFOLOWER)));
+    public static final Item CAULIFLOWER = regitsterItem("cauliflower", new Item(new Item.Settings().food(ModFoodComponents.CAULIFOLOWER)) {
+        @Override
+        public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+            tooltip.add(Text.translatable("tooltip.sabers.cauliflower"));
+            super.appendTooltip(stack, context, tooltip, type);
+        }
+    });
 
     public static final Item STARLIGHT_ASHES = regitsterItem("starlight_ashes", new Item(new Item.Settings()));
 
--- src/main/java/net/ztigerr/saber/item/custom/ChiselItem.java
+++ src/main/java/net/ztigerr/saber/item/custom/ChiselItem.java
@@ -2,17 +2,22 @@ package net.ztigerr.saber.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 import net.ztigerr.saber.block.ModBlocks;
 
+import java.util.List;
 import java.util.Map;
 
 public class ChiselItem extends Item {
@@ -44,5 +49,15 @@ public class ChiselItem extends Item {
             }
         }
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("tooltip.sabers.chisel.shift_down"));
+        }else{
+            tooltip.add(Text.translatable("tooltip.sabers.chisel"));
+        }
+        super.appendTooltip(stack, context, tooltip, type);
     }
 }
