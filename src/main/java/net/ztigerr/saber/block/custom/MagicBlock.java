83bcf07c0af2321be365f5230564a52e272f04e1
ztigerR <regitz66@gmail.com>
2025-01-02 14:19:45 -0500

    Custom Tags Tutorial

--- src/main/java/net/ztigerr/saber/block/custom/MagicBlock.java
+++ src/main/java/net/ztigerr/saber/block/custom/MagicBlock.java
@@ -17,6 +17,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.ztigerr.saber.item.ModItems;
+import net.ztigerr.saber.util.ModTags;
 
 import java.util.List;
 
@@ -35,12 +36,16 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity){
-            if(itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET) {
+            if(isValidItem(itemEntity.getStack())) {
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
             }
         }
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
---
+++ src/main/java/net/ztigerr/saber/util/ModTags.java
@@ -0,0 +1,25 @@
+package net.ztigerr.saber.util;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.ztigerr.saber.Sabers;
+
+public class ModTags {
+    public static class Blocks {
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(Sabers.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        public static final TagKey<Item> TRANSFORMABLE_ITEMS = createTag("transformable_items");
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of(Sabers.MOD_ID, name));
+        }
+    }
+
+}
